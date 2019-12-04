74a3ce936128407cf6dfc49bc115ae818728ce3f
bujesse <bu.jesse@gmail.com>
2019-12-03 20:19:22 -0600

    master - add string representation for models

--- data_visualizer/models/bokeh_graph.py
+++ data_visualizer/models/bokeh_graph.py
@@ -5,10 +5,16 @@ class Country(models.Model):
     country_name = models.CharField(max_length=255)
     country_code = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.country_name
+
 
 class Series(models.Model):
     series_name = models.CharField(max_length=255)
     series_code = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.series_name
 
 
 class DataPoint(models.Model):
